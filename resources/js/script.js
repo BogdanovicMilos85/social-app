fc67c21d27dfdc81f64391a2d1823bbf28ebbf65
milosbogdanovic <milos.bogdanovic1985@gmail.com>
2018-10-03 01:33:59 +0200

    adding new code

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,6 @@
 {
-	let users;
-    const app = document.getElementById('root');
+	const app = document.getElementById('root');
+
 	const logo = document.createElement('img');
 	logo.src = 'resources/images/logo.png';
 
@@ -13,65 +13,102 @@
 
 	app.appendChild(logo);
 	app.appendChild(container);
-
-	const request = new XMLHttpRequest();
-	const url = 'data.json';
-
-	request.open('GET', url, true);
-	request.onload = function() {
-		
-		if (request.status >= 200 && request.status < 400) {
-			users.data = JSON.parse(this.response);
-			getData(users.data)
-		} else {
-			const errorMessage = document.createElement('marquee');
-			errorMessage.textContent = `Something went wrong!`;
-			app.appendChild(errorMessage);
+	
+	class User {
+		constructor(id, name, surname, age, gender, friends) {
+			this.id = id;
+			this.name = name;
+			this.surname = surname;
+			this.age = age;
+			this.gender = gender;
+			this.friends = friends;
 		}
 	}
+	
+	const users = [];
 
-	request.send();
-	
-	
-	let card, h2, p, p2, btn;
-	
-	const ids = [];
-	const usersAll = [];
-	const friends = [];
-	
-	
-	function getData(ourData) {
-		ourData.forEach(cur => {
-            usersAll.push(cur);
-			card = document.createElement('div');
+	let request = new XMLHttpRequest();
+
+	request.open('GET', 'data.json', true);
+	request.onload = function () {
+
+	  // Begin accessing JSON data here
+	  const data = JSON.parse(this.response);
+	  if (request.status >= 200 && request.status < 400) {
+		data.forEach(cur => {
+			let card = document.createElement('div');
 			card.setAttribute('class', 'card');
 
-			h2 = document.createElement('h2');
+			let h2 = document.createElement('h2');
 			h2.textContent = cur.firstName + " " + cur.surname;
 
-			p = document.createElement('p');
+			let p = document.createElement('p');
 			p.textContent = `Age: ${cur.age}`;
 
-			p2 = document.createElement('p');
+			let p2 = document.createElement('p');
 			p2.textContent = `Gender: ${cur.gender}`;
-				
-			btn = document.createElement('button');
-			btn.setAttribute('id', 'btn');
+			
+			let p3 = document.createElement('p');
+			p3.setAttribute('class', 'para');
+			p3.textContent = `Friends with: `;
+
+			let btn = document.createElement('a');
+			btn.setAttribute('class', 'btn');
 			btn.textContent = 'Find more';
+			
+			let list = document.createElement('ul');
 
 			container.appendChild(card);
 			card.appendChild(h2);
 			card.appendChild(p);
 			card.appendChild(p2);
-			card.appendChild(btn);
-            	
+			card.appendChild(p3);
+//			card.appendChild(btn);
+//			
+//			btn.onclick = function() {
+//				btn.setAttribute('href', 'https://google.com');
+//			}
+			
+			let friendsArr = cur.friends;
+			let id = cur.id;
+			let name = cur.firstName; 
+			let surname = cur.surname;
+			let age = cur.age;
+			let gender = cur.gender;
+
+			
+			let friends = friendsArr.map(friendId => {
+//				console.log("friendID is: ", friendId);
+				let friend = data.find(cur => {
+					return cur.id === friendId;
+				});
+				
+            	let listItem = document.createElement('li');
+				listItem.textContent = `${friend.firstName} ${friend.surname}`;
+				list.appendChild(listItem);
+				
+				return {
+					firstName: friend.firstName,
+					surname: friend.surname
+				};	
+			});
+			
+			card.appendChild(list);
+			
+			const allUsers = new User(id, name, surname, age, gender, friends);
+            users.push(allUsers);
+		
 		});
+	  } else {
+			const errorMessage = document.createElement('marquee');
+			errorMessage.textContent = `Something went wrong!`;
+			app.appendChild(errorMessage);
+	  }
+		
+		console.log(users);
 	}
-	
-    document.body.addEventListener('click', function(event) {
-        if(event.srcElement.id == 'btn') {
-           console.log('Test!');       
-        }
-    });
+
+	request.send();
+
 }
 
